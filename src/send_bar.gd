d4fef36ce12251b0cdc60788e9767800f6c4b965
Riley Lyman <rileylyman@gmail.com>
2026-05-05 11:53:12 -0400

    begin work on bar

---
+++ src/send_bar.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@export var fill_time := 1.0
+
+var t := 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- src/send_to_server_button.gd
+++ src/send_to_server_button.gd
@@ -2,14 +2,20 @@ extends TextureButton
 
 @onready var http_request: HTTPRequest = $HTTPRequest
 
+signal sent(success: bool)
+
 func _on_pressed() -> void:
 	var image: Image = get_parent().get_image()
 	var png_bytes: PackedByteArray = image.save_png_to_buffer()
 	http_request.request_completed.connect(func(result: int, response_code: int, _headers: PackedStringArray, _body: PackedByteArray):
-		print("result: ", result)
-		print("response_code: ", response_code)
+		if result == HTTPRequest.RESULT_SUCCESS and response_code == 201:
+			sent.emit(true)
+		else:
+			print("Error when sending image: ", result, " ", response_code)
+			sent.emit(false)
 	)
-	# var req = http_request.request_raw("http://67.205.182.218/images", ["Content-Type: image/png"], HTTPClient.METHOD_POST, png_bytes)
-	var req = http_request.request_raw(FoodManager.ServerAddr, ["Content-Type: image/png"], HTTPClient.METHOD_POST, png_bytes)
-	get_parent()._reset_canvas()
-	#await req.request_completed
+
+	http_request.request_raw(FoodManager.ServerAddr, ["Content-Type: image/png"], HTTPClient.METHOD_POST, png_bytes)
+	var success = await sent
+	if success:
+		get_parent()._reset_canvas()
